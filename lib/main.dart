b777c982beece890394a97aabafb0e570cb32ea2
Maruf Mobin <mobinh264@gmail.com>
2025-10-16 23:35:33 +0600

    All Functionality Done Due only Show or Print All Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:zulzana_air_travel/app.dart';
 
-void main() => runApp(const MyApp());
+void main( ) => runApp( const MyApp());
+
+//
+// import 'package:flutter/material.dart';
+// import 'package:advanced_searchable_dropdown/advanced_searchable_dropdown.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+//
+// import 'package:zulzana_air_travel/model/airport_model.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   List<AirportModel> airportData = [];
+//
+//   Future<void> fetchData() async {
+//     final response = await http.get(
+//       Uri.parse(
+//         'https://enterpise.s3.ap-southeast-1.amazonaws.com/resources/airport.json',
+//       ),
+//     );
+//     if (response.statusCode == 200) {
+//       final List jsonList = jsonDecode(response.body);
+//       airportData = jsonList
+//           .map((e) => AirportModel.fromJson(e))
+//           .toList();
+//     } else {
+//       throw Exception('Some Network issue when fetching Data form Api');
+//     }
+//   }
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchData();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('Searchable Dropdown')),
+//         body: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: SearchableDropDown(
+//
+//             // menuList: [
+//             //   SearchableDropDownItem(label: "Apple", value: 1),
+//             //   SearchableDropDownItem(label: "Banana", value: 2),
+//             //   SearchableDropDownItem(label: "Cherry", value: 3),
+//             //   SearchableDropDownItem(label: "Date", value: 4),
+//             // ],
+//             menuList: airportData.map((e) => SearchableDropDownItem(label: e.airportName.toString(), value: 1)).toList(),
+//             value: 1,
+//             hintText: "Select a fruit",
+//             onSelected: (item) {
+//               print("Selected: ${item.label}");
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/ui/screens/booking_screen.dart
+++ lib/ui/screens/booking_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:zulzana_air_travel/controller/airport_controller.dart';
 import 'package:zulzana_air_travel/model/airport_model.dart';
 import 'package:zulzana_air_travel/ui/widgets/background_screen.dart';
+import 'package:advanced_searchable_dropdown/advanced_searchable_dropdown.dart';
 
 class BookingScreen extends StatefulWidget {
   const BookingScreen({super.key});
@@ -13,25 +14,57 @@ class BookingScreen extends StatefulWidget {
 class _BookingScreenState extends State<BookingScreen> {
   // Airport Controller are call here
   final AirportController _airportController = AirportController();
+  TextEditingController controller = TextEditingController();
+  String query = '';
 
   // Fetch data are stay in array
   List<AirportModel> _airportData = [];
 
-  bool _isLoading = true;
+  // there are some use able variables
+  String? _fromAirport;
+  String? _toAirport;
+
+  DateTime? _selectedDate;
+  int _passengerCount = 1;
+
+  // List of airport name
+  // List<String> airportList = _airportData.map((airport) =>
+  // airport.airportName ?? '').toList();
 
   // Data Fetch are here by function
   Future<void> loadAirportData() async {
     final data = await _airportController.fetchAirportData();
     setState(() {
       _airportData = data;
-      _isLoading = false;
+
+    });
+  }
+
+  void _swapAirports() {
+    setState(() {
+      final temp = _fromAirport;
+      _fromAirport = _toAirport;
+      _toAirport = temp;
     });
   }
 
   @override
   void initState() {
-    loadAirportData();
     super.initState();
+    loadAirportData();
+  }
+
+  void _pickDate() async {
+    final now = DateTime.now();
+    final picked = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: now,
+      lastDate: DateTime(now.year + 2),
+    );
+    if (picked != null) {
+      setState(() => _selectedDate = picked);
+    }
   }
 
   @override
@@ -45,24 +78,212 @@ class _BookingScreenState extends State<BookingScreen> {
             children: [
               SizedBox(height: 32),
               _buildTopButtonGroup(),
-              SizedBox(height: 32),
-              _isLoading
-                  ? Center(child: CircularProgressIndicator())
-                  : Text('Data find'),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _airportData.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: Text(_airportData.first.countryName.toString()),
-                    );
-                  }
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: SearchableDropDown(
+                  menuList: _airportData
+                      .map(
+                        (e) => SearchableDropDownItem(
+                          label: e.airportName.toString(),
+                          value: e.airportName
+                              .toString(), // make value same type as _fromAirport
+                        ),
+                      )
+                      .toList(),
+                  value: _fromAirport,
+                  hintText: "Select From Airport",
+                  onSelected: (item) {
+                    setState(() {
+                      _fromAirport = item.value; // use value here
+                    });
+                    // print("From Airport: ${item.value}");
+                  },
                 ),
               ),
+              IconButton(
+                icon: const Icon(
+                  Icons.swap_vert_outlined,
+                  color: Colors.green,
+                  size: 32,
+                ),
+                onPressed: _swapAirports,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: SearchableDropDown(
+                  menuList: _airportData
+                      .map(
+                        (e) => SearchableDropDownItem(
+                          label: e.airportName.toString(),
+                          value: e.airportName
+                              .toString(), // same type as _toAirport
+                        ),
+                      )
+                      .toList(),
+                  value: _toAirport,
+                  hintText: "Select To Airport",
+                  onSelected: (item) {
+                    setState(() {
+                      _toAirport = item.value;
+                    });
+                    // print("To Airport: ${item.value}");
+                  },
+                ),
+              ),
+              SizedBox(height: 32),
+              _buildPassengerSelector(),
+              _buildDatePicker(),
+
+              // _buildAirportSelector(),
+              // SizedBox(height: 32),
+              // _isLoading
+              //     ? Center(child: CircularProgressIndicator())
+              //     :
+              //
+              // Column(
+              //   children: [
+              //     TextField(
+              //       controller: controller,
+              //       decoration: InputDecoration(hintText: 'Search Airport'),
+              //       onChanged: (value) {
+              //         setState(() => query = value);
+              //       },
+              //     ),
+              //     Expanded(
+              //       child: ListView(
+              //         children: airportList
+              //             .where((airport) =>
+              //             airport.toLowerCase().contains(query.toLowerCase()))
+              //             .map((airport) => ListTile(title: Text(airport)))
+              //             .toList(),
+              //       ),
+              //     ),
+              //   ],
+              // );
+              // _buildAirportSelector(),
+              // Expanded(
+              //   child: ListView.builder(
+              //     itemCount: _airportData.length,
+              //     itemBuilder: (context, index) {
+              //       return ListTile(
+              //         title: Text(_airportData[index].airportName.toString()),
+              //       );
+              //     },
+              //   ),
+              // ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  // Widget _buildAirportSelector() {
+  //   return Row(
+  //     children: [
+  //       Expanded(
+  //         child: DropdownButtonFormField<AirportModel>(
+  //           value: _fromAirport,
+  //           hint: const Text('From'),
+  //           items: _airportData
+  //               .map(
+  //                 (a) => DropdownMenuItem(
+  //                   value: a,
+  //                   child: Text('${a.airportName} (${a.code})'),
+  //                 ),
+  //               )
+  //               .toList(),
+  //           onChanged: (value) => setState(() => _fromAirport = value),
+  //         ),
+  //       ),
+  //       IconButton(
+  //         icon: const Icon(Icons.swap_horiz, color: Colors.green, size: 32),
+  //         onPressed: _swapAirports,
+  //       ),
+  //       Expanded(
+  //         child: DropdownButtonFormField<AirportModel>(
+  //           value: _toAirport,
+  //           hint: const Text('To'),
+  //           items: _airportData
+  //               .map(
+  //                 (a) => DropdownMenuItem(
+  //                   value: a,
+  //                   child: Text('${a.airportName} (${a.code})'),
+  //                 ),
+  //               )
+  //               .toList(),
+  //           onChanged: (value) => setState(() => _toAirport = value),
+  //         ),
+  //       ),
+  //     ],
+  //   );
+  // }
+  void _selectPassengers() async {
+    int tempCount = _passengerCount;
+    await showModalBottomSheet(
+      context: context,
+      builder: (_) => StatefulBuilder(
+        builder: (context, setStateModal) {
+          return Container(
+            padding: const EdgeInsets.all(16),
+            height: 200,
+            child: Column(
+              children: [
+                const Text(
+                  'Select Passengers',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        if (tempCount > 1) setStateModal(() => tempCount--);
+                      },
+                      icon: const Icon(Icons.remove),
+                    ),
+                    Text('$tempCount', style: const TextStyle(fontSize: 20)),
+                    IconButton(
+                      onPressed: () {
+                        setStateModal(() => tempCount++);
+                      },
+                      icon: const Icon(Icons.add),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    setState(() => _passengerCount = tempCount);
+                  },
+                  child: const Text('Done'),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildDatePicker() {
+    return ListTile(
+      onTap: _pickDate,
+      leading: const Icon(Icons.calendar_today, color: Colors.green),
+      title: Text(
+        _selectedDate == null
+            ? 'Select Departure Date'
+            : '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}',
+      ),
+    );
+  }
+
+  Widget _buildPassengerSelector() {
+    return ListTile(
+      onTap: _selectPassengers,
+      leading: const Icon(Icons.person, color: Colors.green),
+      title: Text('Passengers: $_passengerCount'),
     );
   }
 
@@ -126,6 +347,5 @@ class _BookingScreenState extends State<BookingScreen> {
       centerTitle: true,
     );
   }
-
   // App Bar are end here
 }
